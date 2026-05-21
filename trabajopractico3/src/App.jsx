816d00a56e17f22c093fa30b0a1aec7a55696c87
Mateo Miranda <mateojmiranda04@gmail.com>
2026-05-21 19:46:08 -0300

    Consultas APIRest, búsquedas - error

--- trabajopractico3/src/App.jsx
+++ trabajopractico3/src/App.jsx
@@ -1,28 +1,56 @@
 import { useEffect, useState } from "react";
 import axios from 'axios'
+import { getPokemon } from "./services/pokeapi";
 import './App.css'
 
 function App() {
   
   const [pokemon, setPokemon] = useState(null);
+  const [busqueda, setBusqueda] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const buscarPokemon = () => {
+    if (!busqueda) return;
 
-  useEffect(() => {
-    axios.get("https://pokeapi.co/api/v2/pokemon/mew")
+    setLoading(true);
+    setError(false);
+
+    axios.get(`https://pokeapi.co/api/v2/pokemon/${busqueda.toLowerCase()}`)
       .then((response) => {
-        console.log(response);
         setPokemon(response.data);
       })
-  
       .catch((error) => {
-        console.log("Error:", error);
+        console.log(error);
+        setError(true);
+        setPokemon(null);
+      })
+      .finally(() => {
+        setLoading(false);
       });
-  }, []);
+  };
 
   return (
     <div>
       <h1>PokeAPI Test</h1>
 
-      {pokemon && (
+      <input
+        type="text"
+        placeholder="Nombre o ID"
+        value={busqueda}
+        onChange={(e) =>
+          setBusqueda(e.target.value)
+        }
+      />
+
+      <button onClick={buscarPokemon}>
+        Buscar
+      </button>
+
+      {loading && <p>Cargando...</p>}
+
+      {error && <p>Error: Pokémon no encontrado</p>}
+
+      {pokemon && !loading && !error && (
         <div>
           <h2>{pokemon.name}</h2>
 
@@ -30,7 +58,7 @@ function App() {
             src={pokemon.sprites.front_default}
             alt={pokemon.name}
           />
-
+          <p>ID: #{pokemon.id}</p>
           <p>Altura: {pokemon.height}</p>
           <p>Peso: {pokemon.weight}</p>
         </div>
